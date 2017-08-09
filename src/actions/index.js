f663ef9dc382881fa1fc0ce18adbb9ec601833d2
CharDale Barnes <chbarnes@hayneedle.com>
2017-08-09 14:32:42 -0500

    Set up promise for returning get requests through axios (npm install --save axios). Added api key and created a way to store the request data.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,18 @@
+// npm install --save axios
+import axios from 'axios';
+
 // weather api key
 const API_KEY = '29fd3127898293fcf654ece3f0914d6f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+// action creator to perform api request: 
+export function fetchWeather(city,) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    // return your promise from axios: 
+    const request = axios.get(url);
+    // always return an action:
+    return{
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
