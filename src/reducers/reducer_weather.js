21e1a6e15ac66a2d8d52c87a5caf3f5565ea1d5f
CharDale Barnes <chbarnes@hayneedle.com>
2017-08-09 16:41:22 -0500

    Added return for payload data, so that multiple location's weather data can be viewed on the same screen. There are a few ways to do this. I have noted 2-3 ways in the reducer_weather file

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,20 @@
 // reducers are just functions!
-
-export default function(state= null, action ) {
+import { FETCH_WEATHER } from '../actions/index';
+//      store the weather data in an array, for easier access:
+export default function(state= [], action ) {
     // if you want to inspect the reducer, uncomment here: 
     // console.log('Action received', action);
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // handle the payload: 
+            // action.payload.data is the only xhr response node that we care about:
+            //  we want to store the array into the state, but remember the big nono from react: 
+            //      dont do something like state.push(action.payload.data);
+                //      we don't want to update the state directly, use setState() or concat
+                //          or: [ action.payload.data, ...state ]
+            // return state.concat([action.payload.data]); or
+            // same thing: creates a new array without mutating the existing state. 
+            return [ action.payload.data, ...state ];
+    }
     return state;
 }
